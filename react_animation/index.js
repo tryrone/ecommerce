353e1ed9004f307ec8511b3c099fb73124330b76
Tega Oboraruvwe <tega.oboraruvwe@hollaport.net>
2022-01-19 22:27:44 +0100

    all resources

--- react_animation/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import SwipeAnimation from './SwipeAnimation';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => SwipeAnimation);
